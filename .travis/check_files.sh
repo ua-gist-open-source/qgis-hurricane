5940e8a47215ca06b8d7bda9212259910d029225
Aaryn Olsson <aaryn@email.arizona.edu>
2019-10-27 01:37:50 -0700

    Update check_files.sh

--- .travis/check_files.sh
+++ .travis/check_files.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 ret=0
-for f in $(cat required_files.txt); do 
-  if [ ! -r ../$f ]; then 
+cd "$(dirname "$0")"
+for f in $(cat required_files.txt); do
+  if [ ! -r $f ]; then
      >&2 echo "file $f does not exist or is not readable"
      ret=1
   fi
